5308fd8faf521c9306f9d2946199a8b5da793106
Abhijit Barman <113384779+Abhijit0Barman@users.noreply.github.com>
2024-01-10 00:17:38 +0530

    lol

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <div className="bg-blue-800 py-6 ">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">Destiny.com</Link>
+        </span>
+        <span className='flex space-x-2'>
+          <Link to={"/sign-up"} className="text-white font-bold px-4 py-2 rounded-md bg-blue-600 hover:bg-blue-700">Sign Up</Link>
+          <Link to={"/log-in"} className="text-white font-bold px-4 py-2 rounded-md bg-blue-600 hover:bg-blue-700">Log In</Link>
+          <Link to={"/about"} className="text-white font-bold px-4 py-2 rounded-md bg-blue-600 hover:bg-blue-700">About</Link>
+        </span>
+      </div>
+    </div>
+  )
+}
---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header"
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col max-h-screen">
+      <Header />
+    </div>
+  )
+}
+
+export default Layout
